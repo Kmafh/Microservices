b32d995200632b08ba8ae30dc0b311afe806b7e8
unknown <kmafh>
2022-08-29 20:50:13 +0200

    Curso pendiente de Añadir relacion entre cursos y alñumnos v 4.27

--- cursos/src/main/java/com/talan/microservicios/app/cursos/CursosApplication.java
+++ cursos/src/main/java/com/talan/microservicios/app/cursos/CursosApplication.java
@@ -2,8 +2,11 @@ package com.talan.microservicios.app.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.tala.microservicios.app.usuarios.models.entity.alumno",
+	"com.talan.microservicios.app.cursos.models.entity"})
 public class CursosApplication {
 
 	public static void main(String[] args) {
